e68b5a57ff5e5dc9caf69c46195ac4d70eeb46dc
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-08 12:53:05 +0530

    Fix syntax error in urls.py - properly close urlpatterns bracket

--- ecommerce-shop/shop/urls.py
+++ ecommerce-shop/shop/urls.py
@@ -6,10 +6,12 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-# API and Backend views are handled separately    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+
+        path('', TemplateView.as_view(template_name='index.html'), name='home'),
         path('cart.html', TemplateView.as_view(template_name='cart.html'), name='cart'),
     path('checkout.html', TemplateView.as_view(template_name='checkout.html'), name='checkout'),
     path('bill.html', TemplateView.as_view(template_name='bill.html'), name='bill'),
+    ]
         # Order Management API Endpoints
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
