4ab73c057dded269e98700f9bd8fe13341d4ecb9
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 04:20:07 +0530

    Fix admin crash: remove invalid ProductReview import

--- ecommerce-shop/products/admin.py
+++ ecommerce-shop/products/admin.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from .models import Category, Product, Order, OrderItem
 
 
-# ----------------------
-# CATEGORY ADMIN
-# ----------------------
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
 
 
-# ----------------------
-# PRODUCT ADMIN
-# ----------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "price", "stock_quantity", "is_available")
@@ -21,18 +15,11 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-# ----------------------
-# ORDER ITEM INLINE
-# ----------------------
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
-    readonly_fields = ("product", "quantity", "price_at_purchase")
 
 
-# ----------------------
-# ORDER ADMIN
-# ----------------------
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("order_id", "customer_name", "total_amount", "status", "created_at")
@@ -41,9 +28,6 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
 
 
-# ----------------------
-# ORDER ITEM ADMIN
-# ----------------------
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ("id", "order", "product", "quantity", "price_at_purchase")
