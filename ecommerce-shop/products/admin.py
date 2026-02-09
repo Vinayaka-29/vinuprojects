1f1790fcc6ab74d87cb596acb4d35b1f0c530b64
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 18:05:01 +0530

    Update admin.py

--- ecommerce-shop/products/admin.py
+++ ecommerce-shop/products/admin.py
@@ -1,27 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product, Order, OrderItem
+from .models import Product, Order, OrderItem
 
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "stock_quantity", "is_available")
-    list_filter = ("is_available",)
-    search_fields = ("name",)
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ("order_id", "customer_name", "total_amount", "status", "created_at")
-    list_filter = ("status",)
-    search_fields = ("order_id", "customer_name", "customer_email")
-    inlines = [OrderItemInline]
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
