d0413b4eb411928521290b0b6c1aa7794d818825
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 23:35:25 +0530

    Update views.py

--- ecommerce-shop/products/views.py
+++ ecommerce-shop/products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product, Order
 
-def home(request):
-    return render(request, "index.html")
+def index(request):
+    products = Product.objects.all()
+    return render(request, "index.html", {"products": products})
 
-def order_tracking_page(request):
-    return render(request, "index.html")
+
+def order_detail(request, order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+        return render(request, "order_detail.html", {"order": order})
+    except Order.DoesNotExist:
+        return render(request, "order_not_found.html")
