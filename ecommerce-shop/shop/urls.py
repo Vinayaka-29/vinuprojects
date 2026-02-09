a557fa85a530039c8a736777e7547cfd9eda87f2
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 17:06:31 +0530

    Update urls.py

--- ecommerce-shop/shop/urls.py
+++ ecommerce-shop/shop/urls.py
@@ -6,11 +6,13 @@ from products import api
 urlpatterns = [
     path("admin/", admin.site.urls),
 
+    # Pages
     path("", TemplateView.as_view(template_name="index.html")),
     path("cart.html", TemplateView.as_view(template_name="cart.html")),
     path("checkout.html", TemplateView.as_view(template_name="checkout.html")),
     path("bill.html", TemplateView.as_view(template_name="bill.html")),
 
+    # APIs
     path("api/orders/create/", api.create_order),
     path("api/orders/<str:order_id>/", api.get_order_by_id),
 ]
