9dc20b19f2ae247b6111fa2b3fab284c312c5ac5
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 23:36:22 +0530

    Update urls.py

--- ecommerce-shop/shop/urls.py
+++ ecommerce-shop/shop/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from products import views, api
+from products import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home),
-    path("track-order/", views.order_tracking_page),
-    path("api/orders/create/", api.create_order),
+    path("", views.index, name="home"),
+    path("order/<int:order_id>/", views.order_detail),
 ]
