192e56e899693d33c3ee92f9accfefeda415020a
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-10 00:06:48 +0530

    Update urls.py

--- ecommerce-shop/shop/urls.py
+++ ecommerce-shop/shop/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from products import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="home"),
-    path("order/<int:order_id>/", views.order_detail),
+    path("", include("products.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
